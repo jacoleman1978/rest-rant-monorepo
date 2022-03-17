26be4bc193a5c414c9c9071d1cbe83d5cd13b3e4
Jamie Coleman <j.a.coleman1978@gmail.com>
2022-03-16 19:24:23 -0500

    Setup using sessions for authentication

--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -1,12 +1,20 @@
-import { createContext, useState } from "react";
-
+import { createContext, useState, useEffect } from "react";
 
 export const CurrentUser = createContext()
 
 function CurrentUserProvider({ children }){
 
-    const [currentUser, setCurrentUser] = useState(null)
-    window.setCurrentUser = setCurrentUser;
+    const [currentUser, setCurrentUser] = useState(null);
+    useEffect(() => {
+        const getLoggedInUser = async () => {
+            let response = await fetch('http://localhost:4321/authentication/profile', {
+                credentials: 'include'
+            });
+            let user = await response.json()
+            setCurrentUser(user);
+        }
+        getLoggedInUser()
+    }, [])
 
     return (
         <CurrentUser.Provider value={{ currentUser, setCurrentUser }}>
